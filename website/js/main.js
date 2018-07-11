b5d31ec80c686dac8e645f2dcbff10bc8319c2f6
Gav Hanna <gavhanna@gmail.com>
2018-07-11 18:37:04 +0100

    added particle backgrounds

---
+++ website/js/main.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", event => {
+  console.log("DOM fully loaded and parsed");
+
+  particlesJS.load('particles-js', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+  });
+  particlesJS.load('particles-js2', 'assets/particles2.json', function () {
+    console.log('callback - particles.js config loaded');
+  });
+});
+
+
+
+// Smooth scrolling
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000, function () {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
